aefe9bd8c9745a67756be0aafe8f9d0b7dc64a64
Leandro <thesquallrock@gmail.com>
2021-02-09 18:10:08 +0100

    bonus

--- main.js
+++ main.js
@@ -1,14 +1,36 @@
 var app = new Vue ({
   el: '#root',
   data: {
-    diskList: ''
+    diskList: '',
+    genreList: [],
+    selectedGenre: 'All'
   },
   mounted() {
     axios
     .get('https://flynn.boolean.careers/exercises/api/array/music')
     .then((result) => {
-      this.diskList = result.data.response
-      console.log(this.diskList);
+      this.diskList = result.data.response;
+      for (var i = 0; i < this.diskList.length; i++) {
+        if (!this.genreList.includes(this.diskList[i].genre)) {
+          this.genreList.push(this.diskList[i].genre);
+        }
+      };
+      this.diskList.forEach((e, i) => {
+        e.visible = true;
+      });
+
     });
+  },
+  methods: {
+    diskFilter(){
+      this.diskList.forEach((e, i) => {
+        if (this.selectedGenre == e.genre || this.selectedGenre == 'All') {
+          e.visible = true;
+        } else if (this.selectedGenre != e.genre) {
+          e.visible = false;
+        }
+      });
+
+    }
   }
 });

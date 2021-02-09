8fad77e82fb5226def1a47fa94d196b477df7fd7
Leandro <thesquallrock@gmail.com>
2021-02-09 17:20:20 +0100

    stampa a schermo

--- main.js
+++ main.js
@@ -1,3 +1,14 @@
-$(document).ready(function() {
-	//Code
+var app = new Vue ({
+  el: '#root',
+  data: {
+    diskList: ''
+  },
+  mounted() {
+    axios
+    .get('https://flynn.boolean.careers/exercises/api/array/music')
+    .then((result) => {
+      this.diskList = result.data.response
+      console.log(this.diskList);
+    });
+  }
 });
